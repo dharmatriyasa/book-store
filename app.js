9188dfa046210fce210b04ef397a03c42c78d10b
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-01-30 02:05:09 +0800

    route handling 1

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Hello there!');
-    next();
+app.use( '/home', (req, res, next) => {
+    console.log('Hello there is Home Page!');
+    res.send("<h1>Hello there is Home Page!</h1>")
 });
 
-app.use((req, res, next) => {
-    console.log('Hello there 1!');
+app.use('/', (req, res, next) => {
+    console.log('Hello there!');
     res.send("<h1>Hello from my express.js </h1>");
 });
 
