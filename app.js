8700474b030f343812eff6cb5f96946665d5fe42
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-01-30 20:06:19 +0800

    parsing request express.js 2

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use( '/home', (req, res, next) => {
-    console.log('Hello there is Home Page!');
-    res.send("<h1>Hello there is Home Page!</h1>")
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use( '/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit">Add </button> </form>')
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('Hello there!');
     res.send("<h1>Hello from my express.js </h1>");
 });
 
