005d66d45425829d12a8faed71f05a8d8fbb3006
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-05 01:15:36 +0800

    change style css 2

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const producst = [];
+
 router.get( '/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    producst.push({ title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.producst = producst;
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,12 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.producst);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
