2a98dfb0f2ce2ba5a02d8627bbf840df11af4a42
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-07 00:17:40 +0800

    ejs part 1

--- routes/admin.js
+++ routes/admin.js
@@ -8,10 +8,18 @@ const router = express.Router();
 
 const producst = [];
 
+// GET METHOD
 router.get( '/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
 
+// POST METHOD
 router.post('/add-product', (req, res, next) => {
     producst.push({ title: req.body.title});
     res.redirect('/');
--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,15 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.producst);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.producst;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShoo: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
