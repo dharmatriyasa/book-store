3dcd927231c29129727658e5b9ef368d107dd4a9
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-11 02:10:25 +0800

    json file models

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = class Product {
     constructor(title){
@@ -6,10 +8,34 @@ module.exports = class Product {
     }
 
     save(){
-        products.push(this);
+        const pathFile = path.join(
+            path.dirname(require.main.filename),
+            'data',
+            'product.json'            
+        );
+        fs.readFile(pathFile, (err, fileContent) => {
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(pathFile, JSON.stringify(products) , err => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const pathFile = path.join(
+            path.dirname(require.main.filename),
+            'data',
+            'product.json'            
+        );
+        fs.readFile(pathFile, (err, fileContent) =>{
+            if(err){
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 }
