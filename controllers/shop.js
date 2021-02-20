36040e89d7997761a106bec3817408d213c2c9a7
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-21 00:55:07 +0800

    product detail contoller

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,7 +15,9 @@ exports.getAllProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    console.log(prodId);
+    Product.findById(prodId, product => {
+        console.log(product);
+    });
     res.redirect('/');
 }
 
