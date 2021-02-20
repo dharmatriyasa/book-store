f92f9b5edae6432826bad3bd24d241f7b096c471
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-20 23:40:01 +0800

    dynamic routes

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getAllProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
