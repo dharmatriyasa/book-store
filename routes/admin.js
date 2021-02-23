ded7bb883a729652eeabb8ade268ad0a2b5f4726
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-02-23 23:49:34 +0800

    query param

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,11 @@ router.get( '/add-product', adminController.getAddProduct);
 // admin/products
 router.get('/products', adminController.getProducts);
 
+//admin/edit-product
+router.get('/admin/edit-product/:productId', adminController.getEditProduct);
+
 // POST METHOD
+
 router.post('/add-product', adminController.postAddProduct);
 
 exports.router = router;
