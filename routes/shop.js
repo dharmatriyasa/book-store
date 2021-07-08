f9ac841f0086474e1376ea5ff2641fcf99e7f904
dharmatriyasa <dharmatriyasa19@gmail.com>
2021-07-09 02:08:26 +0700

    fixin bug

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const { count } = require('../models/order-item');
 
 const router = express.Router();
 
@@ -16,10 +17,10 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.post('/create-order', shopController.postOrder);
 
 router.get('/orders', shopController.getOrders);
-
-router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
